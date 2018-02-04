fc90fb55c9b0fef1cd82603c1d2c405a03d7dc48
wangkaiwd <1162486176@qq.com>
2018-02-04 16:29:32 +0800

    react生命周期

--- usewebpack/react-进阶/条件渲染/src/index.js
+++ usewebpack/react-进阶/条件渲染/src/index.js
@@ -1,26 +1,45 @@
 import React,{Component} from 'react';
 import {render} from 'react-dom';
 
+// return null可以阻止组件渲染
+// class WaringBanner extends Component {
+//     constructor() {
+//         super()
+//     }
+//     componentWillMount() {
+//         this.handleWarning();
+//     }
+//     handleWarning() {
+//         if(!this.props.warn) {
+//             return null;
+//         }
+//     } 
+//     render() {
+//         return (
+//             <div>
+//                 warning
+//             </div>
+//         )
+//     }
+// }
+
 class IfRender extends Component {
-    constructor() {
-        super() 
+    constructor(props) {
+        super(props) 
     }
-
-
     render() {
         return (
             <div>
                 <h1>条件渲染</h1>
 
-                {/* expression:表达式
+                {/* 
+                expression:表达式：
                     true && 内容      返回内容，显式内容
                     false && 内容     返回false，不显示内容
-                
                     true || 内容      返回true，不显示内容
                     false || 内容     返回内容，显式内容
-                
                 */}
-                {false && <p>&&的理解</p>}
+                {true && <p>&&的理解</p>}
             </div>
         )
     }
