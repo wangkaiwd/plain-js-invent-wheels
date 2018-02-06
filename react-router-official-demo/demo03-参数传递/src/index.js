6718e6f52345f5b35edd51105c2bb51b4a02c38e
wangkaiwd <1162486176@qq.com>
2018-02-06 19:07:14 +0800

    react组件创建的方法

--- react-router-official-demo/demo03-参数传递/src/index.js
+++ react-router-official-demo/demo03-参数传递/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {Router,hashHistory} from 'react-router'; 
+import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import routes from './router/routes';
 
 render(
